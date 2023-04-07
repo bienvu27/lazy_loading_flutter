0c67f494545178c0c76d2c32770b39f8e19d4050
bienvu27 <bienvx1999@gmail.com>
2023-04-07 15:45:59 +0700

    first update

---
+++ android/app/src/main/kotlin/com/example/lazy_loading_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lazy_loading_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  static int page = 0;
+  final ScrollController _sc = ScrollController();
+  bool isLoading = false;
+  List users = [];
+  final dio = Dio();
+
+  @override
+  void initState() {
+    _getMoreData(page);
+    super.initState();
+    _sc.addListener(() {
+      if (_sc.position.pixels ==
+          _sc.position.maxScrollExtent) {
+        _getMoreData(page);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _sc.dispose();
+    super.dispose();
+  }
+
+  void _getMoreData(int index) async {
+    if (!isLoading) {
+      setState(() {
+        isLoading = true;
+      });
+      var url = "https://randomuser.me/api/?page=$index&results=20&seed=abc";
+      print(url);
+      final response = await dio.get(url);
+      List tList = [];
+      for (int i = 0; i < response.data['results'].length; i++) {
+        tList.add(response.data['results'][i]);
+      }
+
+      setState(() {
+        isLoading = false;
+        users.addAll(tList);
+        page++;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        title: const Text("Lazy Load Large List"),
+      ),
+      body: Container(
+        child: _buildList(),
+      ),
+
+    );
+  }
+
+  Widget _buildList() {
+    return ListView.builder(
+      itemCount: users.length + 1, // Add one more item for progress indicator
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      itemBuilder: (BuildContext context, int index) {
+        if (index == users.length) {
+          return _buildProgressIndicator();
+        } else {
+          return ListTile(
+            leading: CircleAvatar(
+              radius: 30.0,
+              backgroundImage: NetworkImage(
+                users[index]['picture']['large'],
+              ),
+            ),
+            title: Text((users[index]['name']['first'])),
+            subtitle: Text((users[index]['email'])),
+          );
+        }
+      },
+      controller: _sc,
+    );
+  }
+
+  Widget _buildProgressIndicator() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: Opacity(
+          opacity: isLoading ? 1.0 : 00,
+          child: const CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
